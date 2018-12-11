00561f1e968c09b11e5405d577e4aa0d80bc0925
Ömer YILMAZEL <omeryilmazel@gmail.com>
2018-12-11 23:05:04 +0300

    fix

--- components/Brick.js
+++ components/Brick.js
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
-import Injector from 'react-native-injectable-component';
+import React, { Component } from "react";
+import { View, Image, TouchableHighlight } from "react-native";
+import Injector from "react-native-injectable-component";
 
-export default function Brick (props) {
-	// Avoid margins for first element
-	const image = (props.onPress) ? _getTouchableUnit(props, props.gutter) : _getImageTag(props, props.gutter);
-	const footer = (props.renderFooter) ? props.renderFooter(props.data) : null;
-	const header = (props.renderHeader) ? props.renderHeader(props.data) : null;
+export default function Brick(props) {
+  // Avoid margins for first element
+  const image = props.onPress
+    ? _getTouchableUnit(props, props.gutter)
+    : _getImageTag(props, props.gutter);
+  const footer = props.renderFooter ? props.renderFooter(props.data) : null;
+  const header = props.renderHeader ? props.renderHeader(props.data) : null;
 
-	return (
-		<View key={props.brickKey}>
-		  {header}
-		  {image}
-		  {footer}
-		</View>
-	);
+  return (
+    <View key={props.brickKey}>
+      {header}
+      {image}
+      {footer}
+    </View>
+  );
 }
 
 // _getImageTag :: Image, Gutter -> ImageTag
-export function _getImageTag (props, gutter = 0) {
-	const imageProps = {
-		key: props.uri,
-		source: {
-			uri: props.uri
-		},
-		data: {
-			...props.data
-		},
-		resizeMethod: 'auto',
-		style: {
-			width: props.width,
-			height: props.height,
-			marginTop: gutter,
-			...props.imageContainerStyle,
-		}
-	};
+export function _getImageTag(props, gutter = 0) {
+  const imageProps = {
+    key: props.uri,
+    source: {
+      uri: props.uri
+    },
+    data: {
+      ...props.data
+    },
+    resizeMethod: "auto",
+    style: {
+      width: props.width,
+      height: props.height,
+      marginTop: gutter,
+      ...props.imageContainerStyle
+    }
+  };
 
-	return (
-		<Injector
-		  defaultComponent={Image}
-		  defaultProps={imageProps}
-		  injectant={props.customImageComponent}
-		  injectantProps={props.customImageProps} />
-	)
+  return (
+    <Injector
+      defaultComponent={Image}
+      defaultProps={imageProps}
+      injectant={props.customImageComponent}
+      injectantProps={props.customImageProps}
+    />
+  );
 }
 
 // _getTouchableUnit :: Image, Number -> TouchableTag
-export function _getTouchableUnit (image, gutter = 0) {
-	return (
-		<TouchableOpacity
-          key={image.key}
-          onPress={() => image.onPress(image.data)}
-	  activeOpacity={0.7}
-	  style={{ borderRadius: 8 }}>
-          <View>
-            { _getImageTag(image, gutter) }
-          </View>
-		</TouchableOpacity>
-	);
+export function _getTouchableUnit(image, gutter = 0) {
+  return (
+    <TouchableHighlight
+      key={image.uri}
+      onPress={() => image.onPress(image.data)}
+      underlayColor={"white"}
+      style={{ borderRadius: 8 }}
+    >
+      <View>{_getImageTag(image, gutter)}</View>
+    </TouchableHighlight>
+  );
 }
